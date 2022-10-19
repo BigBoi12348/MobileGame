77feae9004b49fcbe151c9ea7f3a0f6bcc2da229
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2022-10-19 21:15:25 +1100

    gam

---
+++ angrybirds-main/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public LineRenderer[] lineRenderers;
+    public Transform[] stripPositions;
+    public Transform center;
+    public Transform idlePosition;
+
+    public Vector3 currentPosition;
+
+    public float maxLength;
+
+    public float bottomBoundary;
+
+    bool isMouseDown;
+
+    public GameObject ballPrefab;
+
+    public float ballPositionOffset;
+
+    Rigidbody2D ball;
+    Collider2D ballCollider;
+
+    public float force;
+
+    void Start()
+    {
+        lineRenderers[0].positionCount = 2;
+        lineRenderers[1].positionCount = 2;
+        lineRenderers[0].SetPosition(0, stripPositions[0].position);
+        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+
+        CreateBird();
+    }
+
+    void CreateBird()
+    {
+        ball = Instantiate(ballPrefab).GetComponent<Rigidbody2D>();
+        ballCollider = ball.GetComponent<Collider2D>();
+        ballCollider.enabled = false;
+
+        ball.isKinematic = true;
+
+        ResetStrips();
+    }
+
+    void Update()
+    {
+        if (isMouseDown)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+
+            currentPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            currentPosition = center.position + Vector3.ClampMagnitude(currentPosition
+                - center.position, maxLength);
+
+            currentPosition = ClampBoundary(currentPosition);
+
+            SetStrips(currentPosition);
+
+            if (ballCollider)
+            {
+                ballCollider.enabled = true;
+            }
+        }
+        else
+        {
+            ResetStrips();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isMouseDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isMouseDown = false;
+        Shoot();
+        currentPosition = idlePosition.position;
+    }
+
+    void Shoot()
+    {
+        ball.isKinematic = false;
+        Vector3 birdForce = (currentPosition - center.position) * force * -1;
+        ball.velocity = birdForce;
+
+        ball.GetComponent<Bird>().Release();
+
+        ball = null;
+        ball = null;
+        Invoke("CreateBird", 2);
+    }
+
+    void ResetStrips()
+    {
+        currentPosition = idlePosition.position;
+        SetStrips(currentPosition);
+    }
+
+    void SetStrips(Vector3 position)
+    {
+        lineRenderers[0].SetPosition(1, position);
+        lineRenderers[1].SetPosition(1, position);
+
+        if (ball)
+        {
+            Vector3 dir = position - center.position;
+            ball.transform.position = position + dir.normalized * ballPositionOffset;
+            ball.transform.right = -dir.normalized;
+        }
+    }
+
+    Vector3 ClampBoundary(Vector3 vector)
+    {
+        vector.y = Mathf.Clamp(vector.y, bottomBoundary, 1000);
+        return vector;
+    }
+}
